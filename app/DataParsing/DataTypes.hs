d699a4089a2931c1c40f64dce8c06153e3668ad7
peppapig450 <nbgenius1@gmail.com>
2024-05-14 10:53:40 -0700

    remove excess stuff we dont need

--- app/DataParsing/DataTypes.hs
+++ app/DataParsing/DataTypes.hs
@@ -7,11 +7,7 @@ import Data.Aeson
 data UpComingEarning = UpComingEarning {
     date :: String,
     symbol :: String,
-    eps :: Maybe Double,
-    epsEstimated :: Maybe Double,
     time :: Maybe String,
-    revenue :: Maybe Float,
-    revenueEstimated :: Maybe Float,
     fiscalDateEnding :: String,
     updatedFromDate :: String
 } deriving (Show)
@@ -20,10 +16,6 @@ instance FromJSON UpComingEarning where
     parseJSON = withObject "UpComingEarning" $ \v -> UpComingEarning
         <$> v .:  "date"
         <*> v .:  "symbol"
-        <*> v .:? "eps"
-        <*> v .:? "epsEstimated"
         <*> v .:? "time"
-        <*> v .:? "revenue"
-        <*> v .:? "revenueEstimated"
         <*> v .: "fiscalDateEnding"
         <*> v .: "updatedFromDate"
