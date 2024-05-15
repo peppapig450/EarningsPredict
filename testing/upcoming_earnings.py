84881a9f7b5b217410a57f0c5e197cbb13e4fee9
peppapig450 <nbgenius1@gmail.com>
2024-05-15 10:31:23 -0700

    python testing

---
+++ testing/upcoming_earnings.py
@@ -0,0 +1,11 @@
+import finnhub
+import json
+
+finnhub_client = finnhub.Client(api_key="cp238p9r01qhquut3u7gcp238p9r01qhquut3u80")
+
+upcoming_earnings = finnhub_client.earnings_calendar(
+    _from="2024-05-15", to="2024-05-18", symbol="", international=False
+)
+
+symbols = set(earning["symbol"] for earning in upcoming_earnings["earningsCalendar"])
+print(symbols)
---
+++ testing/upcoming_earnings2.py
@@ -0,0 +1,45 @@
+import fmpsdk
+import json
+import re
+
+
+def get_upcoming_earnings(api_key, from_date, to_date):
+    earnings_data = fmpsdk.earning_calendar(
+        apikey=api_key, from_date=from_date, to_date=to_date
+    )
+
+    filtered_symbols = []
+    for earning in earnings_data:
+        symbol = earning.get("symbol")
+        # Filter if symbol exists, is a string, and doesn't end with country code extension
+        if symbol and not re.search(r"\.[A-Z]*$", symbol):
+            filtered_symbols.append(symbol)
+
+    return filtered_symbols
+
+
+def get_historical_earnings_data(api_key, filtered_symbols):
+    historical_earnings_data = {}
+    for symbol in filtered_symbols:
+        historical_earnings = fmpsdk.historical_earning_calendar(
+            apikey=api_key, symbol=symbol
+        )
+        print(f"{symbol}\n: {historical_earnings}")
+        historical_earnings_data[symbol] = historical_earnings
+
+    return historical_earnings_data
+
+
+if __name__ == "__main__":
+    fmp_api_key = "1Bo8cqgwItUyx0emLvZOFv2GGX20Bbmm"
+
+    upcoming_earnings_symbols = get_upcoming_earnings(
+        fmp_api_key, "2024-05-16", "2024-05-18"
+    )
+
+    historical_earnings = get_historical_earnings_data(
+        fmp_api_key, upcoming_earnings_symbols
+    )
+
+    with open("historical_data.json", "w") as file:
+        json.dump(historical_earnings, file, indent=4)
